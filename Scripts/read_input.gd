a6b0614402a4a17b5e3e44422ee609c9eb53fdc6
Arvind Maan <arvindsmaan@gmail.com>
2021-01-09 16:51:43 -0600

    Fix attack animation

--- Scripts/read_input.gd
+++ Scripts/read_input.gd
@@ -79,28 +79,34 @@ func check_words():
 	for enemy in enemies.get_children():
 		var prompt = enemy.get_prompt()
 		if prompt == typed_buffer:
+			kill_enemy()
+			yield(sprite, "animation_finished")
+			start_running()
+
+			# actually kill the enemy now
 			enemy.queue_free()
 			typed_buffer = ""
 			buffer_label.text = typed_buffer
-			kill_enemy()
 			break
-
-func kill_enemy():
-	enemies_killed += 1
-
-	for bg in scrolling_bg.get_children():
-		bg.stop_archer()
-	sprite.play("Attack")
-		
-	if enemies_killed >= current_wave_size:
-		stop_wave()
-		total_enemies_killed += enemies_killed
-		enemies_killed = 0
 
 func start_running():
 	for bg in scrolling_bg.get_children():
 		bg.start_archer()
-	sprite.play("Run")
+		sprite.play("Run")
+
+func stop_running():
+	for bg in scrolling_bg.get_children():
+		bg.stop_archer()
+
+func kill_enemy():
+	enemies_killed += 1
+	stop_running()
+	sprite.play("Attack")
+	
+	if enemies_killed >= current_wave_size:
+		stop_wave()
+		total_enemies_killed += enemies_killed
+		enemies_killed = 0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and not event.is_pressed():
@@ -134,9 +140,3 @@ func spawn_enemy():
 	enemy_instance.init(direction, enemy_speed, min_word_length, max_word_length)
 	enemies.add_child(enemy_instance)
 	enemy_instance.global_position = spawns[index].global_position
-
-
-func _on_archer_sprite_animation_finished():
-	for bg in scrolling_bg.get_children():
-		bg.start_archer()
-	sprite.play("Run")
