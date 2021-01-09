2ae5f107fee31186af67d715705208632a86d5d2
Arvind Maan <arvindsmaan@gmail.com>
2021-01-08 22:11:42 -0600

    Create enemy scene

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,8 +1,13 @@
 extends AnimatedSprite
 
+export (float) var speed = 0.1
+
+# get the text
 onready var prompt = $target
-# get the text from the target
 onready var prompt_text = prompt.text
+
+func _physics_process(delta):
+	global_position.x -= speed
 
 func get_prompt() -> String:
 	return prompt_text
