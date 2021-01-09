267766fd75dee734d29af75fddba010b92c76fb7
Caden Chabot <chabotc@myumanitoba.ca>
2021-01-09 13:34:14 -0600

    Changed typing to keep a buffer and match all words

--- Scripts/read_input.gd
+++ Scripts/read_input.gd
@@ -6,40 +6,35 @@ onready var enemies = $enemies
 onready var r_spawn_points = $enemy_right_spawns
 onready var l_spawn_points = $enemy_left_spawns
 onready var spawn_timer = $spawn_timer
+onready var buffer_label = $buffer as RichTextLabel
 
-var curr_letter_index: int = -1
-var active_enemy = null
+var typed_buffer = ""
+var active_words = []
 
 func _ready() -> void:
 	randomize()
 	spawn_enemy()
 	spawn_timer.start()
 	
-	
-func find_new_enemy(typed_char: String):
+func checkWords():
 	for enemy in enemies.get_children():
 		var prompt = enemy.get_prompt()
-		if prompt.substr(0, 1) == typed_char:
-			active_enemy = enemy
-			curr_letter_index = 1
-			print("enemy targeted")
-	
+		if prompt == typed_buffer:
+			enemy.queue_free()
+			typed_buffer = ""
+			buffer_label.text = typed_buffer
+			
+		
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and not event.is_pressed():
 		var type_event = event as InputEventKey
-		var key_typed = char(type_event.scancode).to_lower()
-		if active_enemy == null:
-			find_new_enemy(key_typed)
+		if type_event.scancode == KEY_BACKSPACE:
+			typed_buffer = typed_buffer.substr(0, typed_buffer.length() - 1)
+			buffer_label.text = typed_buffer
 		else:
-			var prompt = active_enemy.get_prompt()
-			var next_char = prompt.substr(curr_letter_index, 1)
-			if key_typed == next_char:
-				curr_letter_index += 1
-				if curr_letter_index == prompt.length():
-					curr_letter_index = -1
-					active_enemy.queue_free()
-					active_enemy = null
-					print("enemy killed")
+			typed_buffer += char(type_event.scancode).to_lower()
+			buffer_label.text = typed_buffer
+			checkWords()
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_enemy()
