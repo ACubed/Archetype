fa6ca31a34373dd7bf024a157db78c7647005ee8
Caden Chabot <chabotc@myumanitoba.ca>
2021-01-09 20:56:02 -0600

    killing enemies when they are done attacking arvind loves this message

--- Scripts/read_input.gd
+++ Scripts/read_input.gd
@@ -55,12 +55,15 @@ func start_game():
 	spawn_enemy()
 	start_wave()
 	archer_container.add_child(archer_obj)
+	archer_obj.health = 100
 	health_label.text = "Health: %d" % archer_obj.health
 	get_node("archer/archer_sprite").playing = true
 	initialize_music()
 	started = true
+	game_over = false
 
 func stop_world():
+	spawn_timer.stop()
 	for enemy in enemies.get_children():
 		enemy.queue_free()
 	sprite.play("Death")
@@ -80,6 +83,7 @@ func _process(delta):
 					enemy.attack()
 					yield(enemy.sprite, "animation_finished")
 					archer_obj.take_hit(enemy.hit_points)
+					enemy.queue_free()
 					if archer_obj.health <= 0:
 						stop_world()
 						
